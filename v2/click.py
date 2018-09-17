7b942496ba8a99118bf610332d26f45fa1eee944
zhangminglu <1367000465@qq.com>
2018-09-17 22:01:14 +0800

    使用MySQL经行存储

--- v2/click.py
+++ v2/click.py
@@ -1,8 +1,13 @@
 # -*-coding: utf-8 -*-
 
+import time
+import random
+import threading
 
 import pymysql
-from v2.setting import mysql_config
+import requests
+
+from v2.setting import mysql_config, TIME_DELAY_1, TIME_DEALY_2
 
 
 def get_article_id_list():
@@ -16,3 +21,30 @@ def get_article_id_list():
     return article_id_list
 
 
+def request_get_url(article_url):
+    requests.get(article_url)
+
+
+def main():
+    url = r'https://blog.csdn.net/zhang_Ming_lu/article/details/'
+    article_id_list = get_article_id_list()
+    num = random.randint(0, len(article_id_list))
+    flag = True
+    while flag:
+        try:
+            article_url = url + str(article_id_list[num])
+        except Exception:
+            article_url = url + str(article_id_list[-1])
+        num += 1
+        if num == len(article_id_list):
+            num = 0
+            time.sleep(TIME_DELAY_1)
+        print(article_url)
+        t = threading.Thread(target=request_get_url, args=(article_url,))
+        t.start()
+        time.sleep(TIME_DEALY_2)
+    print('运行结束！！！')
+
+
+if __name__ == "__main__":
+    main()
--- v2/setting.py
+++ v2/setting.py
@@ -15,4 +15,10 @@ mysql_config = {
 PAGE_RANGE = 3
 # 每个文章的url的 xpath
 ARTICLE_URL_XPATH = r'//h4/a'
+ARTICLE_TITLE_XPATH = r'//h4/a//text()'
 BASE_URL = r'https://blog.csdn.net/zhang_ming_lu/article/list/{page_num}'
+
+# 将所有文章的url全部请求一次的时间间隔
+TIME_DELAY_1 = 60
+# 每个url请求之间的时间间隔
+TIME_DEALY_2 = 5
